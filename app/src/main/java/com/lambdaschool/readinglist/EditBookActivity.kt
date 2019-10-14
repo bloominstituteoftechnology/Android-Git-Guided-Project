2b99c18556113797481a2d81e92ed500319e97ff
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-14 11:51:24 -0600

    updated colors

--- app/src/main/java/com/lambdaschool/readinglist/EditBookActivity.kt
+++ app/src/main/java/com/lambdaschool/readinglist/EditBookActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+
 import kotlinx.android.synthetic.main.activity_edit_book.*
 
 /*
